7c76103766657fa45ac200bd4fcbaaac46430a63
Alexey <podzakon11@gmail.com>
2021-07-31 22:02:51 +0300

    created even_odd_prime function

---
+++ Task_1/main.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+def even_odd_prime(numbers: List[int], type_of_out='prime') -> List[int]:
+    '''Takes list of intergers and returns filtered numbers
+       by type[prime, odd, even]'''
+
+    if type_of_out == 'even':
+        return list(filter(lambda x: x % 2 == 0, numbers))
+
+    if type_of_out == 'odd':
+        return list(filter(lambda x: x % 2 != 0, numbers))
+
+    if type_of_out == 'prime':
+        primes = []
+        for number in numbers:
+            if number <= 1:
+                continue
+            for i in range(2, number):
+                if number % i == 0:
+                    break
+            else:
+                primes.append(number)
+        return primes
+
+    return None
