507c4ae2e337fffafefe880d083a829e4f865a84
Alexey <podzakon11@gmail.com>
2021-08-03 12:43:18 +0300

    Vehicle refactored

--- Task_2/vehicle.py
+++ Task_2/vehicle.py
@@ -1,6 +1,12 @@
 '''Vehicle'''
 from abc import ABC, abstractmethod
-from vehicle_errors import WeightTypeError
+from vehicle_errors import (WeightTypeError,
+                            PositiveError,
+                            CarryingTypeError,
+                            MileageTypeError,
+                            NameTypeError,
+                            EmptyStringError,
+                            ProductionYearTypeError)
 
 
 class Vehicle(ABC):
@@ -10,51 +16,74 @@ class Vehicle(ABC):
                  name: str,
                  weight: float,
                  carrying: float,
-                 production_year: int
+                 production_year: int,
+                 mileage: float
                  ):
+
+        if not isinstance(name, str):
+            raise NameTypeError
+
+        if not name:
+            raise EmptyStringError
+
         self._name = name
+
+        if not isinstance(weight, (int, float)):
+            raise WeightTypeError
+
+        if weight < 0:
+            raise PositiveError(weight)
+
         self._weight = weight
+
+        if not isinstance(carrying, (int, float)):
+            raise CarryingTypeError
+
+        if carrying < 0:
+            raise PositiveError(carrying)
+
         self._carrying = carrying
+
+        if not isinstance(production_year, int):
+            raise ProductionYearTypeError
+
+        if production_year < 0:
+            raise PositiveError(production_year)
+
         self._production_year = production_year
+
+        if not isinstance(mileage, (int, float)):
+            raise MileageTypeError
+
+        if mileage < 0:
+            raise PositiveError(mileage)
+
+        self._mileage = mileage
 
     @property
     def name(self):
         '''name getter'''
         return self._name
 
-    @name.setter
-    def name(self, name):
-        self._name = name
-
     @property
     def weight(self):
         '''weight getter'''
         return self._weight
-
-    @weight.setter
-    def weight(self, weight):
-        if not isinstance(self.weight, float):
-            raise WeightTypeError
-
-        self._weight = weight
 
     @property
     def carrying(self):
         '''carrying getter'''
         return self._carrying
 
-    @carrying.setter
-    def carrying(self, carrying):
-        self._carrying = carrying
-
     @property
     def production_year(self):
         '''production_year getter'''
         return self._production_year
 
-    @production_year.setter
-    def production_year(self, production_year):
-        self._production_year = production_year
+    @property
+    def mileage(self):
+        '''mileage getter'''
+        return self._mileage
 
     @abstractmethod
     def move(self, distance: float):
