361ae16a04f9a9afa421f80e29e16065e39cbd1d
Alexey <podzakon11@gmail.com>
2021-08-03 12:41:03 +0300

    Added EmpyString, Positive, Type errors

--- Task_2/vehicle_errors.py
+++ Task_2/vehicle_errors.py
@@ -3,9 +3,41 @@
 
 class WeightTypeError(TypeError):
     '''WeightTypeError'''
-    pass
 
 
-class PositiveWeightError(ValueError):
-    '''PositiveWeightError'''
-    pass
+class CarryingTypeError(TypeError):
+    '''CarryingTypeError'''
+
+
+class MileageTypeError(TypeError):
+    '''MileageTypeError'''
+
+
+class ProductionYearTypeError(TypeError):
+    '''ProductionYearTypeError'''
+
+
+class NameTypeError(TypeError):
+    '''NameTypeError'''
+
+
+class VinTypeError(TypeError):
+    '''VinTypeError'''
+
+
+class ModelTypeError(TypeError):
+    '''ModelTypeError'''
+
+
+class PositiveError(ValueError):
+    '''PositiveError'''
+    def __init__(self, value, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__value = value
+
+    def __str__(self):
+        return f'Value can\'t be negative. Input {self.__value}'
+
+
+class EmptyStringError(ValueError):
+    '''EmptyStringError'''
